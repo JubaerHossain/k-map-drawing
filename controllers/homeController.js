dc3d6afa27ff89839d89a8d7423135967e518325
jubaer.sookh <jubaer@sookh.com>
2022-03-31 18:44:00 +0600

    sample data

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,8 @@
 const validateFileUploadInput = require('../validations/fileUpload');
 const Uploads = require('../models/uploadModel');
+const csv = require("csvtojson");
+const fs = require('fs');
+
 
 let fileUpload = (req, res, next) => { 
     try {
@@ -7,19 +10,72 @@ let fileUpload = (req, res, next) => {
             if(!isValid) {
                 req.app.set('errors', errors)
                 return res.redirect("/");
-            }    
-            var upload = new Uploads(data)
-            Uploads.addUpload(upload, (err, upload) => {
-                if (err) {
-                    req.app.set('errors', err)
-                    return res.redirect("/");
-                } else {
-                    return res.redirect("/download?file=" + upload?._id);
+            }
+
+            const sampleFile = {}
+            let location_details
+            let location_employee_details
+            let employee_details
+            let colleges_location_values
+            csv()
+            .fromFile(`./uploads/${data?.employee_details}`)
+            .then((jsonObj)=>{
+                employee_details = jsonObj;
+
+                var location_details = JSON.parse(fs.readFileSync(`./uploads/${data?.location_details}`, 'utf8'));
+                var location_employee_details = JSON.parse(fs.readFileSync(`./uploads/${data?.location_employee_details}`, 'utf8'));
+                var colleges_location_values = JSON.parse(fs.readFileSync(`./uploads/${data?.colleges_location_values}`, 'utf8'));
+                // console.log('employee_details');
+                // console.log(employee_details);
+                // console.log('location_details');
+                // console.log(location_details);
+                // console.log('location_employee_details');
+                // console.log(location_employee_details);
+                // console.log('colleges_location_values');
+                // console.log(colleges_location_values);
+                //  merge the location_details  to get  employee_details and location_employee_details and colleges_location_values   dynamically
+                
+
+
+                
+
+                for(let i=0; i<location_details.length; i++){
+                    for(let j=0; j<location_employee_details.length; j++){
+                        if(location_details[i].location_id === location_employee_details[j].location_id){
+                            location_details[i].location_employee_details = location_employee_details[j];
+                        }
+                    }
                 }
-            });
-            return res.redirect("/download?file=" + upload?._id);
+                console.log('location_details');
+                console.log(location_details['features']);
+                console.log(location_details['features'][0].geometry);
+                //  merge the location_details and location_employee_details to get the location_employee_details_merged dynamically
+
+                 
+                
+                
+                
+                console.log(sampleFile);
+ 
+                
+            }).catch((err)=>{
+                console.log(err);
+            })
+
+            // var upload = new Uploads(data)
+            // Uploads.addUpload(upload, (err, upload) => {
+            //     if (err) {
+            //         req.app.set('errors', err)
+            //         return res.redirect("/");
+            //     } else {
+
+            //         return res.redirect("/download?file=" + upload?._id);
+            //     }
+            // });
+            // return res.redirect("/download?file=" + upload?._id);
     }
     catch(err) {
+        process.exit(0);
         req.app.set('errors', err)
         return res.redirect("/");
     }      
